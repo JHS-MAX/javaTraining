e6140a6f56b476663fb551c16b266363131d561d
user <user@DESKTOP-A2F4MA8>
2019-09-15 20:25:37 +0900

    첫 commit

---
+++ src/BAEKJOON_2588.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class BAEKJOON_2588 {
+
+	public static void main(String[] args) {
+
+		int number1;
+		String number2;
+		int n1,n2,n3,n4;
+
+		Scanner scan = new Scanner(System.in);
+		number1 = scan.nextInt();
+		number2 = scan.next();
+	
+	}
+
+}
