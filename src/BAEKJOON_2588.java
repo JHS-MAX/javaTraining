77fea6d5508ced0e04533ded12f4dfe6c50091d2
user <user@DESKTOP-A2F4MA8>
2019-09-15 20:47:17 +0900

    두번 째 commit

--- src/BAEKJOON_2588.java
+++ src/BAEKJOON_2588.java
@@ -6,12 +6,24 @@ public class BAEKJOON_2588 {
 
 		int number1;
 		String number2;
-		int n1,n2,n3,n4;
+		int n[] = new int[3];
+		int result;
 
 		Scanner scan = new Scanner(System.in);
+		
 		number1 = scan.nextInt();
 		number2 = scan.next();
 	
+		String str[] = number2.split("");
+		
+		n[0] = number1 * Integer.parseInt(str[2]);
+		n[1] = number1 * Integer.parseInt(str[1]);
+		n[2] = number1 * Integer.parseInt(str[0]);
+		result = number1 * Integer.parseInt(number2);
+		
+		for(int i=0; i<n.length; i++) 
+		System.out.println(n[i]);
+		System.out.println(result);
 	}
 
 }
