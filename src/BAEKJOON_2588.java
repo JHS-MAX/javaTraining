d606af077c6562719cb50b13e5769e28374b1878
user <user@DESKTOP-A2F4MA8>
2019-09-15 20:59:20 +0900

    두번 째 commit

--- src/BAEKJOON_2588.java
+++ src/BAEKJOON_2588.java
@@ -1,3 +1,5 @@
+// 백준알고리즘 2588번 문제 
+
 import java.util.Scanner;
 
 public class BAEKJOON_2588 {
@@ -10,20 +12,19 @@ public class BAEKJOON_2588 {
 		int result;
 
 		Scanner scan = new Scanner(System.in);
-		
+
 		number1 = scan.nextInt();
 		number2 = scan.next();
-	
+
 		String str[] = number2.split("");
-		
+
 		n[0] = number1 * Integer.parseInt(str[2]);
 		n[1] = number1 * Integer.parseInt(str[1]);
 		n[2] = number1 * Integer.parseInt(str[0]);
 		result = number1 * Integer.parseInt(number2);
-		
+
 		for(int i=0; i<n.length; i++) 
-		System.out.println(n[i]);
+			System.out.println(n[i]);
 		System.out.println(result);
 	}
-
 }
