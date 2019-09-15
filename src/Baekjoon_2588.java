747a5a580ececd2c175364d3271e5d2003f402b1
user <user@DESKTOP-A2F4MA8>
2019-09-15 19:57:13 +0900

    s

--- src/Baekjoon_2588.java
+++ src/Baekjoon_2588.java
@@ -4,13 +4,7 @@ public class Baekjoon_2588 {
 
 	public static void main(String[] args) {
 
-		int number1;
-		String number2;
-		int n1,n2,n3,n4;
-
-		Scanner scan = new Scanner(System.in);
-		number1 = scan.nextInt();
-		number2 = scan.next();
+		System.out.println("Hello");
 
 	
 	}
