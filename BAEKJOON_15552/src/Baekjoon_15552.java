ac94d3285b2e5be32dfe3226cf0eff0473d9961e
user <user@DESKTOP-A2F4MA8>
2019-09-23 01:25:06 +0900

    (1) commit

---
+++ BAEKJOON_15552/src/Baekjoon_15552.java
@@ -0,0 +1,24 @@
+// 백준 알고리즘 문제번호 15552
+
+import java.io.*;
+
+public class Baekjoon_15552 {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		int t = Integer.parseInt(br.readLine()); 
+		
+		if(t<=1000000) {
+			for(int i=0; i<t; i++) {
+				
+				
+			}
+			
+		}
+
+	}
+
+}
