a1bc6a372b58ee95b95f250c466b4afc71ec8612
user <user@DESKTOP-A2F4MA8>
2019-09-28 16:10:21 +0900

    (2) Commit

--- BAEKJOON_1110/src/BaekJoon_1110.java
+++ BAEKJOON_1110/src/BaekJoon_1110.java
@@ -9,11 +9,11 @@ public class BaekJoon_1110 {
 		Scanner scan = new Scanner(System.in);
 
 		int start_n = scan.nextInt();  
+		int n = start_n;
 		int count = 0;
 
 		if(0<=start_n && start_n <=99 ) {
 			while(true) {	
-				int n = start_n;
 				int n1 = n%10; // 일의 자리
 				int n2 = n/10; // 십의 자리
 				int n3 = n2+n1; // 일의자리 수 + 십의자리 수
