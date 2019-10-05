c0c1dd30ff8ac5cd23f2aa90b7739d1687e2d696
user <user@DESKTOP-A2F4MA8>
2019-10-05 19:09:50 +0900

    (1) Commit

---
+++ BAEKJOON_10818/src/Main.java
@@ -0,0 +1,20 @@
+// 문제 번호 10818
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		
+		int n = scan.nextInt();
+		int array[] = new int[n];
+		
+		for(int i=0; i<array.length; i++)
+			array[i]=scan.nextInt();
+		
+
+	}
+
+}
