e2a8e567c45ba4269fae8d695455115877f9502d
user <user@DESKTOP-A2F4MA8>
2019-10-05 19:18:22 +0900

    (2) Commit

--- BAEKJOON_10818/src/Main.java
+++ BAEKJOON_10818/src/Main.java
@@ -5,16 +5,26 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		
+
 		Scanner scan = new Scanner(System.in);
-		
+
 		int n = scan.nextInt();
 		int array[] = new int[n];
-		
-		for(int i=0; i<array.length; i++)
-			array[i]=scan.nextInt();
-		
 
+		if(1<=n && n<=1000000) {
+			for(int i=0; i<array.length; i++)
+				array[i]=scan.nextInt();
+		}
+
+		int max = array[0];
+		int min = array[0];
+
+		for(int i=0; i<array.length; i++) {
+			if(array[i] > max)
+				max=array[i];
+			if(array[i] < min)
+				min = array[i];
+		}
 	}
 
 }
