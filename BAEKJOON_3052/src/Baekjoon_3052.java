679e7969e4629fe99d3bd200577ca3bda43b68dd
user <user@DESKTOP-A2F4MA8>
2019-10-10 13:07:45 +0900

    (2) Commit

--- BAEKJOON_3052/src/Baekjoon_3052.java
+++ BAEKJOON_3052/src/Baekjoon_3052.java
@@ -1,4 +1,26 @@
+// 백준 문제 번호 3052
+
+import java.util.Scanner;
 
 public class Baekjoon_3052 {
 
+	public static void main(String args[]) {
+		
+		Scanner scan = new Scanner(System.in);
+		
+		int count = 0;
+		int number[] = new int[10];
+		int remainderOfNumber[] = new int[10];
+		
+		for(int i=0; i<number.length; i++) {
+			number[i] = scan.nextInt();
+			remainderOfNumber[i] = number[i] % 42;
+		}
+		
+		for(int i=0; i<number.length; i++)
+			if(number[i] != remainderOfNumber[i]) count++;
+				
+		System.out.println(count);
+				
+	}	
 }
