060d6710d978c17171c3d2515b7a4130c9395569
user <user@DESKTOP-A2F4MA8>
2019-10-20 09:19:43 +0900

    (1) Commit

---
+++ BAEKJOON_1546/src/Baekjoon_1546.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Baekjoon_1546 {
+	
+	public static void main(String args[]) {
+		
+		Scanner scan = new Scanner(System.in);
+		int N = scan.nextInt();
+		double array[] = new double[N];
+		int max =0;
+		double sum = 0;
+		
+		
+		
+		
+	}
+}
