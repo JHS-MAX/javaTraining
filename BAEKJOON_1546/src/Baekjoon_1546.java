1c391a04eac08f8ca693cc51c18534e0c4d1becc
user <user@DESKTOP-A2F4MA8>
2019-10-20 10:34:21 +0900

    (3) Commi

--- BAEKJOON_1546/src/Baekjoon_1546.java
+++ BAEKJOON_1546/src/Baekjoon_1546.java
@@ -1,28 +1,37 @@
 import java.util.Scanner;
 
 public class Baekjoon_1546 {
-	
+
 	public static void main(String args[]) {
-		
+
 		Scanner scan = new Scanner(System.in);
 		int N = scan.nextInt();
-		double array[] = new double[N];
-		int max =0;
-		double sum = 0;
-		
-		for(int i=0; i<array.length; i++) {
-			array[i] = scan.nextDouble();
-			if(array[max] < array[i]) max = i;
-		}
-		
-		for(int i=0; i<array.length; i++) {
-			array[i] = (array[i]/array[max]) * 100;
-			sum += array[i];
-		}
-		
-		System.out.println(sum/N);
-		
-		
-		
+
+		if(0 < N && N <= 1000) {
+			double score[] = new double[N];
+			double max = 0;
+			double sum = 0;
+
+			for(int i=0; i<score.length; i++) {
+				score[i] = scan.nextInt();
+				if(score[i] > 100 || score[i] < 0) { 
+					System.out.println("성적 입력 허용 범위 초과(0~100)");
+					return;
+				}
+				if(max < score[i]) max = score[i];
+			}
+
+			for(int i=0; i<score.length; i++) {
+				score[i] = score[i]/max * 100;
+				sum += score[i];
+			}
+
+			if(sum == 0) {
+			System.out.println("합=0");
+			return;
+			}
+			System.out.println(sum / N);
+		} else System.out.println("과목의 수는 1000을 초과 할수 없습니다.");
+		System.out.println(0/50);
 	}
 }
