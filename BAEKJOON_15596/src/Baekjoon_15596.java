96aec249d9277c8f2f734535926786aa56e9836c
user <user@DESKTOP-A2F4MA8>
2019-10-28 18:02:51 +0900

    (2) Commit

--- BAEKJOON_15596/src/Baekjoon_15596.java
+++ BAEKJOON_15596/src/Baekjoon_15596.java
@@ -2,22 +2,27 @@ import java.util.Scanner;
 
 public class Baekjoon_15596 {
 
+	public static long sum(int[] a) {
+		long result = 0;
+		for(int i=0; i<a.length; i++)
+			result += a[i];
+		return result;
+	}
+	
 	public static void main(String args[]) {
 
 		Scanner scan = new Scanner(System.in);
 
 		int n = scan.nextInt();
 
-		if(1000000<=n && n<=3000000) {
+		if(1<=n && n<=3000000) {
 			int a[] = new int[n];
-			long sum = 0;
 
 			for(int i=0; i<n; i++) {
 				a[i] = scan.nextInt();
-				sum += a[i];
 			}
 
-			System.out.println(sum);
+			System.out.println(sum(a));
 		}
 
 	}
